15ce979e945596285d743f1e11bda52b0ea80dad
JuanManuelPaezCarrilloComfenalco <paez3939@gmail.com>
2022-11-12 11:34:23 -0500

    todo_sobre_variables_12_11_2022

---
+++ main.js
@@ -0,0 +1,22 @@
+addEventListener("DOMContentLoaded", (e) =>{
+
+let Minombre = "Juan Manuel";
+console.log( Minombre)
+let miApellido="Paez Carrillo";
+console.log( miApellido)
+let Miedad="38 años" 
+console.log( Miedad)
+let miPeso="80 kg"
+console.log(miPeso) 
+let soyProfesor=false
+console.log(soyProfesor)
+let estasEstudiandoJavescript=true
+console.log(estasEstudiandoJavescript)
+let comoSeLlamaElProximoCurso=null
+console.log(comoSeLlamaElProximoCurso) 
+let cuantoEsTuPeso
+console.log(cuantoEsTuPeso) 
+let miFormaDeVestir={camisa:"tipo polo o manga corrta", pantalon: "jean", zapatos: "tennis", accesorios: "reloj"}
+console.log(miFormaDeVestir)
+
+})
